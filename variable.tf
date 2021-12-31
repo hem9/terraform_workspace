161c044e373cbf3e58d2761379d0039294d0fb41
hem9 <39554936+hem9@users.noreply.github.com>
2021-12-31 15:04:54 +0000

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -3,20 +3,21 @@ variable "rules" {
         port = number
         proto = string
         cidr_blocks = list(string)
-    })
-    
+        })
     )
     description = "(optional) describe your variable"
     default = [{
         port = 80
         proto ="tcp"
         cidr_blocks = ["0.0.0.0/0"]
-    },
+    }
+        ,
     {
         port = 22
         proto ="tcp"
         cidr_blocks = ["0.0.0.0/0"]
-    },
+    }
+        ,
     {
         port = 3689
         proto ="tcp"
