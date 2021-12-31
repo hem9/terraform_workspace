080fd984aa667698f74222b33ad95f93bc53b016
hem9 <39554936+hem9@users.noreply.github.com>
2021-12-31 15:41:01 +0000

    Update network.tf

--- network.tf
+++ network.tf
@@ -2,33 +2,34 @@
 resource "aws_vpc" "vpc_master" {
   cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "${terraform.workspace}-VPC"
+    Name = "${terraform.workspace}-vpc"
   }
+
 }
 
-#Get all available AZ's in VPC
+#Get all available AZ's in VPC for master region
 data "aws_availability_zones" "azs" {
   state = "available"
 }
 
-#Create subnet in us-east-1
-/*resource "aws_subnet" "subnet" {
-    aws_availability_zone = element(data.aws_availability_zones.azs.names,0)
-    vpc_id = aws_vpc.vpc_master.id
-    cidr_block = "10.0.1.0/24"
+#Create subnet # 1 in us-east-1
+resource "aws_subnet" "subnet" {
+  availability_zone = element(data.aws_availability_zones.azs.names, 0)
+  vpc_id            = aws_vpc.vpc_master.id
+  cidr_block        = "10.0.1.0/24"
 
-    tags = {
-      Name = "${terraform.workspace}-subnet"  
-    }
-    
-}*/
+  tags = {
+    Name = "${terraform.workspace}-subnet"
+  }
+}
 
-#Create SG for TCP/22 ssh from anywhere
+
+#Create SG for allowing TCP/22 from anywhere, THIS IS FOR TESTING ONLY
 resource "aws_security_group" "sg" {
   name        = "${terraform.workspace}-sg"
-  description = "Allow 22 port"
+  description = "Allow TCP/22"
   vpc_id      = aws_vpc.vpc_master.id
-  dynamic "ingress" {
+dynamic "ingress" {
     for_each = var.rules
     content {
       from_port   = ingress.value["port"]
@@ -42,5 +43,8 @@ resource "aws_security_group" "sg" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "${terraform.workspace}-securitygroup"
   }
 }
