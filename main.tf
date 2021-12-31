5c680f6bd7b4f7dbf56be346fd002a556ec77bc0
hem9 <39554936+hem9@users.noreply.github.com>
2021-12-31 15:38:02 +0000

    corrected main file

--- main.tf
+++ main.tf
@@ -1,16 +1,20 @@
 provider "aws" {
-  region = "us-east-1"
-
+  region = terraform.workspace == "default" ? "us-east-1" : "us-west-2"
 }
 
+#Get Linux AMI ID using SSM Parameter endpoint in us-east-1
+data "aws_ssm_parameter" "linuxAmi" {
+  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+}
 
+#Create and bootstrap EC2 in us-east-1
 resource "aws_instance" "ec2-vm" {
-  ami                         = "ami-0ed9277fb7eb570c9"
+  ami                         = data.aws_ssm_parameter.linuxAmi.value
   instance_type               = "t3.micro"
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.sg.id]
-  user_data                   = fileexists("script.sh") ? file("script.sh") : null
+  subnet_id                   = aws_subnet.subnet.id
   tags = {
-    Name = "workspace-ec2"
+    Name = "${terraform.workspace}-ec2"
   }
 }
